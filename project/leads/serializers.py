a40303ad94a75e66f238d15bb388fe5f4fcf24cc
Jhonbeltran <jbeltranleon@gmail.com>
2019-01-10 21:09:54 -0500

    mapp all the models fields

--- project/leads/serializers.py
+++ project/leads/serializers.py
@@ -3,4 +3,4 @@ from leads.models import Lead
 class LeadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lead
-        fields = ('id', 'name', 'email', 'message')
+        fields = '__all__'
