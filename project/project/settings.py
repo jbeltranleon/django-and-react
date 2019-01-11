da8a49e7d1396239144b8c819e54b0c6f64f2acb
Jhonbeltran <jbeltranleon@gmail.com>
2019-01-10 21:38:05 -0500

    add bd to gitignore

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #Local
+    # Local
     'leads',
+
+    # Third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
